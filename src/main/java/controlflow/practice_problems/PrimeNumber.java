b5ed7c978cea38fcf6b662cc6307b8e1a19c6e51
MOPATI RISHITH REDDY <mr4280@srmist.edu.in>
2026-08-27 14:24:18 +0530

    Add practice problem L1 Prime Number

---
+++ src/main/java/controlflow/practice_problems/PrimeNumber.java
@@ -0,0 +1,30 @@
+package controlflow.practice_problems;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Is the number " + number + " a Prime number? true");
+        } else {
+            System.out.println("Is the number " + number + " a Prime number? false");
+        }
+    }
+}
