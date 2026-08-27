2e3689d1ac8770d0a9e6a260c89a493e64bf1502
MOPATI RISHITH REDDY <mr4280@srmist.edu.in>
2026-08-27 22:00:18 +0530

    Add assingment problem PositiveNegativeZero

---
+++ src/main/java/controlflow/assigment_problems/PositiveNegativeZero.java
@@ -0,0 +1,22 @@
+package controlflow.assignment_problems;
+
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
