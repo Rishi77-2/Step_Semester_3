d6ac537c4c489e5db6c18c8cc8acd66c714c2ee2
MOPATI RISHITH REDDY <mr4280@srmist.edu.in>
2026-09-11 23:23:01 +0530

    add containsDuplicatepractice problem

---
+++ src/main/java/arrays/practice_problems/ContainsDuplicate.java
@@ -0,0 +1,30 @@
+package arrays.practice_problems;
+
+import java.util.Scanner;
+
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] nums = {1, 2, 3, 1};
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
